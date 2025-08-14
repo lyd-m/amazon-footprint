b33c97c8e70bdfa090af1bb8b6590edd14ba5bab
Lydia Marsden <145122215+lyd-m@users.noreply.github.com>
2025-08-14 15:05:48 +0100

    classify issuance and financier locations as domestic/regional/intl
    - calculate totals for each group
    - calculate percentage of flows issued in the focus country, region versus internationally
    - calculate percentage of flows financed from the focus country, region, versus internationally
    - update regions_map to ensure all countries are mapped to standard regions classification
    - output files to analytical-results

--- scripts/7-attribute-financial-data.R
+++ scripts/7-attribute-financial-data.R
@@ -46,29 +46,113 @@ for (file_ in flows_by_manager_files) {
 flows_all_amazon <- flows[["flows_clean_manager_level_2008_2024_all_amazon"]]
 flows[["flows_clean_manager_level_2008_2024_all_amazon"]] <- NULL
 
+# totals for each country_commodity
+
+totals_usd_m <- tibble()
+for (country_commodity in names(flows)){
+  df <- flows[[country_commodity]]
+  if (nrow(df) > 0) {
+    result <- df %>% 
+      group_by(producer_country, commodity) %>%
+      summarise(total_usd_m = sum(tranche_amount_per_manager_usd_m_final_in_dec_2024_usd, na.rm=TRUE))
+    
+    totals_usd_m <- bind_rows(totals_usd_m, result)
+  } else print("Empty df")
+}
+
+write_csv(totals_usd_m, "./analytical-results/flows_total_per_country_commodity.csv")
+
 ## 2. direct / indirect -------------------
 
 ### Issued in producer country? ----------------
+
+# get continental regions for case studies, matching the flows data
+regions_unsd <- read_csv('./input-data/regions_map.csv') %>%
+  select(country, region_unsd)
+
+# add producer regions
+producer_regions <- tibble(
+  "country" = c("Brazil", "Bolivia", "Colombia", "Peru", "Ecuador"),
+  "producer_region" = rep("Latin America and the Caribbean", 5)
+)
+
+# adding classification of where flows are issued
+flows_by_issuance_location_type <- tibble()
 for (country_commodity in names(flows)) {
   print(country_commodity)
   df <- flows[[country_commodity]]
-  if (nrow(df) > 0) {
+  if (nrow(df) > 0) { # skip empty data (Brazil coffee, Brazil maize)
+    df <- df %>%
+      # add producer_region (LatAm)
+      left_join(producer_regions, by = c("producer_country" = "country")) %>%
+      # reclassify issuer regions in line with unsd
+      left_join(regions_unsd, by = c("borrower_issuer_country" = "country")) %>%
+      rename(borrower_issuer_region_unsd = region_unsd) %>%
+      mutate(flow_issuance_location_type = case_when(
+        producer_country == borrower_issuer_country ~ "Domestic",
+        producer_region == borrower_issuer_region_unsd & producer_country != borrower_issuer_country ~ "Regional",
+        TRUE ~ "International"
+      ))
+    
+    flows[[country_commodity]] <- df # add adjusted data back to df
+    
     result <- df %>%
-      mutate(issued_in_producer_country = if_else(
-        producer_country == borrower_issuer_country,
-        "Issued in producer country",
-        "Issued outside producer country"
-      )) %>%
-      group_by(issued_in_producer_country) %>%
+      group_by(flow_issuance_location_type) %>%
       summarise(amount_usd_m = sum(tranche_amount_per_manager_usd_m_final_in_dec_2024_usd, na.rm = TRUE),
                 .groups = "drop") %>%
       mutate(pct = percent(amount_usd_m/sum(amount_usd_m), accuracy = 0.01)) %>%
-      as_tibble()
+      as_tibble() %>% 
+      arrange(factor(flow_issuance_location_type, levels = c("Domestic", "Regional", "International"))) %>%
+      mutate(case = paste0(country_commodity))
     
     print(result, n = Inf) 
     
+    flows_by_issuance_location_type <- bind_rows(flows_by_issuance_location_type, result)
+    
   } else print("Empty df")
 }
+
+write_csv(flows_by_issuance_location_type, "./analytical-results/flows_by_issuance_location_type.csv")
+
+### Financed in producer country? ----------------
+# adding classification of where flows are financed from
+
+flows_by_financed_location_type <- tibble()
+for (country_commodity in names(flows)) {
+  print(country_commodity)
+  df <- flows[[country_commodity]]
+  if (nrow(df) > 0) { # skip empty data (Brazil coffee, Brazil maize)
+    df <- df %>%
+      # reclassify financier regions in line with unsd
+      left_join(regions_unsd, by = c("manager_true_ultimate_parent_country_of_headquarters" = "country")) %>%
+      select(-manager_true_ultimate_parent_region_of_headquarters_unsd) %>%
+      rename(manager_true_ultimate_parent_region_of_headquarters_unsd = region_unsd) %>%
+      mutate(flow_financed_location_type = case_when(
+        producer_country == manager_true_ultimate_parent_country_of_headquarters ~ "Domestic",
+        producer_region == manager_true_ultimate_parent_region_of_headquarters_unsd & producer_country != manager_true_ultimate_parent_country_of_headquarters ~ "Regional",
+        TRUE ~ "International"
+      ))
+    
+    flows[[country_commodity]] <- df # add adjusted data back to df
+    
+    result <- df %>%
+      group_by(flow_financed_location_type) %>%
+      summarise(amount_usd_m = sum(tranche_amount_per_manager_usd_m_final_in_dec_2024_usd, na.rm = TRUE),
+                .groups = "drop") %>%
+      mutate(pct = percent(amount_usd_m/sum(amount_usd_m), accuracy = 0.01)) %>%
+      as_tibble() %>% 
+      arrange(factor(flow_financed_location_type, levels = c("Domestic", "Regional", "International"))) %>%
+      mutate(case = paste0(country_commodity))
+    
+    print(result, n = Inf) 
+    
+    flows_by_financed_location_type <- bind_rows(flows_by_financed_location_type, result)
+    
+  } else print("Empty df")
+}
+
+write_csv(flows_by_financed_location_type, "./analytical-results/flows_by_issuance_location_type.csv")
+
 
 ### Sector issued in ----------------
 trbc_activities <- tibble()
@@ -93,6 +177,12 @@ for (country_commodity in names(flows)) {
 }
 
 write_csv(trbc_activities, "./intermediate-results/trbc_activities_classification.csv")
+## these were then manually assigned as 'direct', 'indirect', and 'minimally-related'
 
-## 3. exclusion around appearance in SEI-Trase/DeDuCE data -------------------
+trbc_activities_classified <- read_excel("./intermediate-results/trbc_activities_classification_final.xlsx") %>%
+  select(-Notes)
+
+## 3. exclusion SEI-Trase/DeDuCE data, all dates -------------------
+
+## 3. exclusion SEI-Trase/DeDuCE data, appearance (i.e., phase-in / phase-out) -------------------
 
